4a51e307f02b79d0b23f43142798e170b1c4d0eb
zigomir <zigomir@gmail.com>
2017-11-10 14:34:37 -0800

    Fix the build.
    Also minify esm format.

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,8 @@ import minify from 'rollup-plugin-babel-minify'
 
 const production = !process.env.ROLLUP_WATCH
 const outputIndex = 8
-const formatIndex = 6
 const ranger = process.argv[outputIndex].includes('nanocal-ranger')
-const iife = process.argv[formatIndex] === 'iife'
+const min = process.argv[outputIndex].includes('.min')
 const cssFileName = ranger ? 'nanocal-ranger' : 'nanocal'
 const outputName = ranger ? 'NanocalRanger' : 'Nanocal'
 
@@ -21,6 +20,6 @@ export default {
       dev: !production, // enable run-time checks when not in production
       css: css => { css.write(`dist/${cssFileName}.min.css`) }
     }),
-    production && iife && minify({ comments: false })
+    production && min && minify({ comments: false })
   ]
 }
