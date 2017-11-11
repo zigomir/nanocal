fde1c24976c011c47c0e889930bdbd073fd6c3ca
zigomir <zigomir@gmail.com>
2017-11-10 16:06:14 -0800

    Keep uppercase first letter convention for iife.

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,8 @@ const production = !process.env.ROLLUP_WATCH
 const outputIndex = production ? 8 : 9 // TODO: make this more robust
 const ranger = process.argv[outputIndex].includes('ranger')
 const min = process.argv[outputIndex].includes('.min')
-const name = ranger ? 'ranger' : 'nanocal'
+const name = ranger ? 'Ranger' : 'Nanocal' // will be used in iife
+const cssName = ranger ? 'ranger' : 'nanocal'
 
 export default {
   output: {
@@ -17,7 +18,7 @@ export default {
     resolve(),
     svelte({
       dev: !production, // enable run-time checks when not in production
-      css: css => { css.write(`dist/${name}.min.css`) }
+      css: css => { css.write(`dist/${cssName}.min.css`) }
     }),
     production && min && minify({ comments: false })
   ]
